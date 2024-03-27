c9943de9cf4c82d787b9af570081c6f9b6961ea6
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-03-27 21:59:25 +0530

    working

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 app.use(cors());
 app.use(express.json());
-const Transaction = require('./models/Transaction,js');
+const Transaction = require('./models/transaction.js');
 const { default: mongoose } = require('mongoose');
 
 
@@ -12,11 +12,24 @@ app.get('/api/test', (req, res) => {
     res.json({ body: 'test ok' });
 });
 
-app.post('/api/transaction',(req, res) => {
-    const {name,description,datetime} = req.body
-    mongoose.connect
-    res.json(req.body)
-})
+app.post('/api/transaction', async (req, res) => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        const { name, description, datetime, price } = req.body;
+        const transaction = await Transaction.create({ name, description, datetime, price });
+        res.json(transaction);
+        console.log("Transaction created:", transaction);
+    } catch (error) {
+        console.error("Error creating transaction:", error);
+        res.status(500).json({ error: 'Failed to create transaction' });
+    }
+});
+
+app.get('/api/transactions',async (req, res) => {
+    await mongoose.connect(process.env.MONGO_URL);
+    const transactions = await Transaction.find({});
+    res.json(transactions)
+});
 
 const port = 4040;
 app.listen(port, () => {
--- src/App.js
+++ src/App.js
@@ -1,73 +1,94 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [name, setName] = useState("");
   const [datetime, setDatetime] = useState("");
   const [description, setDescription] = useState("");
-
-  function addTransaction(){
-    const url = process.env.REACT_APP_API_URL+'/transaction';
-    fetch(url , {
-      method: 'POST',
-      headers: {'Content-type':'application/json'},
-      body:JSON.stringify({name,description,datetime})
-    }).then(response => {
-      response.json().then(json => {console.log('result' + json)})
-    })
+  const [transactions, setTransactions] = useState([]);
+  useEffect(() => {
+    getTransactions().then(setTransactions);
+  }, []);
+  async function getTransactions() {
+    const response = await fetch(
+      process.env.REACT_APP_API_URL + "/transactions"
+    );
+    return await response.json();
   }
 
+  function addTransaction() {
+    const url = process.env.REACT_APP_API_URL + "/transaction";
+    const price = name.split(" ")[0];
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        name: name.substring(price.length + 1),
+        price,
+        description,
+        datetime,
+      }),
+    }).then((response) => {
+      response.json().then((json) => {
+        setName("");
+        setDatetime("");
+        setDescription("");
+        console.log("result" + json);
+      });
+    });
+  }
+  let balance = 0;
+  for (const transaction of transactions) {
+    balance = balance + transaction.price;
+  }
   return (
     <main>
-      <h1>₹100</h1>
-      <form onSubmit={addTransaction}>
-        <div className="basics">
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder={"+200 Food"}
-          />
-          <input
-            type="datetime-local"
-            value={datetime}
-            onChange={(e) => setDatetime(e.target.value)}
-          />
-        </div>
-        <div className="description">
-          <input
-            type="text"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder={"Description"}
-          />
-        </div>
-        <button typeof="submit">Add Transaction</button>
-      </form>
-      <div className="transactions">
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Expense</div>
-            <div className="description">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            </div>
+      <div className="shell">
+        <h1>₹{balance}</h1>
+        <form onSubmit={addTransaction}>
+          <div className="basics">
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder={"+200 Food"}
+            />
+            <input
+              type="datetime-local"
+              value={datetime}
+              onChange={(e) => setDatetime(e.target.value)}
+            />
           </div>
-          <div className="right">
-            <div className="price-expense">-₹200</div>
-            <div className="datetime">123456778</div>
+          <div className="description">
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder={"Description"}
+            />
           </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Income</div>
-            <div className="description">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            </div>
-          </div>
-          <div className="right">
-            <div className="price-income">+₹200</div>
-            <div className="datetime">123456778</div>
-          </div>
+          <button typeof="submit">Add Transaction</button>
+        </form>
+        <div className="transactions">
+          {transactions.length > 0 &&
+            transactions.map((transaction) => (
+              <div className="transaction">
+                <div className="left">
+                  <div className="name">{transaction.name}</div>
+                  <div className="description">{transaction.description}</div>
+                </div>
+                <div className="right">
+                  <div
+                    className={
+                      "price-" + (transaction.price < 0 ? "expense" : "income")
+                    }
+                  >
+                    {transaction.price}
+                  </div>
+                  <div className="datetime">{transaction.name}</div>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     </main>
